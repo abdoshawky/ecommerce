44615b75bd48850c60980cf1804af96bdb11e9d6
Abdelrhman shawky <abdelrhman.shawky@areebgroup.com>
2023-01-30 01:21:10 +0200

    database migrations

--- database/migrations/2023_01_27_214152_create_order_item_table.php
+++ database/migrations/2023_01_27_214152_create_order_item_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('order_item', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('item_id')->constrained()->cascadeOnDelete();
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('order_item');
     }
 };
